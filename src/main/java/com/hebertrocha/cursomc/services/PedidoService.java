3fcfd8604a9233a0add03074d90b16070bdcd885
hebertrocha <hebertrocha2003@gmail.com>
2019-12-19 18:01:36 -0300

    Endpoint/pedidos/{id} disponibilizado

---
+++ src/main/java/com/hebertrocha/cursomc/services/PedidoService.java
@@ -0,0 +1,29 @@
+package com.hebertrocha.cursomc.services;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hebertrocha.cursomc.domain.Pedido;
+import com.hebertrocha.cursomc.repositories.PedidoRepository;
+import com.hebertrocha.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) throws ObjectNotFoundException {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado: Id:" + id + ", Tipo: " + Pedido.class.getName()));
+
+	}
+	
+
+}
