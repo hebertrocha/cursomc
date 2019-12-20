7e5ed0dd8f10a279ffd2da092b4d500a4acb37ef
hebertrocha <hebertrocha2003@gmail.com>
2019-12-20 10:41:36 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/hebertrocha/cursomc/services/CategoriaService.java
+++ src/main/java/com/hebertrocha/cursomc/services/CategoriaService.java
@@ -16,7 +16,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) throws ObjectNotFoundException {
+	public Categoria find(Integer id) throws ObjectNotFoundException {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado: Id:" + id + ", Tipo: " + Categoria.class.getName()));
@@ -28,4 +28,8 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 }
